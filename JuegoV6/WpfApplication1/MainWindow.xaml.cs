3a3fbaee6a311d7d80b4f5e0eb4f8aceffd4f894
emerrefe <mcarmenraflo@gmail.com>
2013-05-19 16:25:10 +0200

    primeras versiones

---
+++ JuegoV6/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+        private LinearGradientBrush colorIni;
+        int contador = 0;
+        List<Image> resultado=new List<Image>(); 
+
+		public MainWindow(Image img)
+		{
+			this.InitializeComponent();
+            this.avatar.Source = img.Source;
+            
+			// A partir de este punto se requiere la inserción de código para la creación del objeto.
+		}
+
+        private void resaltar(object sender, MouseEventArgs e)
+        {
+            colorIni = btnSalir.ColorIniBoton;
+
+            btnSalir.ColorBoton = Brushes.Red;
+        }
+
+        private void desresaltar(object sender, MouseEventArgs e)
+        {
+            btnSalir.ColorIniBoton = colorIni;
+        }
+
+        private void salir(object sender, MouseButtonEventArgs e)
+        {
+            Window1 VentanaInicial = new Window1();
+            VentanaInicial.Show();
+            this.Close();
+        }
+
+        private void inicioArrastrar(object sender, MouseButtonEventArgs e)
+        {
+            DataObject dataO = new DataObject(((Image)sender));
+            DragDrop.DoDragDrop((Image)sender, dataO, DragDropEffects.Move);
+        }
+
+        private void soltar(object sender, DragEventArgs e)
+        {
+            if (contador < 4)
+            {
+                Image aux = (Image)e.Data.GetData(typeof(Image));
+                resultado.Add(aux);
+
+                StackPanel st = new StackPanel();
+                Image im = new Image();
+
+                im.Source = aux.Source;
+                im.Height = 80;
+                st.Children.Add(im);
+
+                listView1.Items.Add(st);
+                contador++;
+            }
+
+        }
+
+        private void comprobar(object sender, RoutedEventArgs e)
+        {
+
+            if (contador == 4)
+            {
+                StackPanel st = new StackPanel();
+                st.Orientation = Orientation.Horizontal;
+                for (int i = 0; i < 4; i++)
+                {
+                    Image im = new Image();
+                    aniadir(st, im, i);
+
+                }
+                listView2.Items.Add(st);
+                listView1.Items.Clear();
+                contador = 0;
+                resultado.Clear();
+            }
+
+        }
+
+        private void aniadir(StackPanel st, Image img, int posicion)
+        {
+            img.Source = resultado[posicion].Source;
+            img.Height = 80;
+            st.Children.Add(img);            
+        }
+	}
+}
---
+++ JuegoV6/WpfApplication1/SelAvatar.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Lógica de interacción para Window1.xaml
+	/// </summary>
+    /// 
+   
+
+	public partial class Window1 : Window
+	{
+        
+        Image imagen = new Image();
+       
+      
+		public Window1()
+		{
+			this.InitializeComponent();
+			
+			// A partir de este punto se requiere la inserción de código para la creación del objeto.
+		}
+
+
+        private void cambiarVentana(object sender, RoutedEventArgs e)
+        {
+            //Evento entrar a la ventana de jugar.
+            if (imagen.Source == null)
+            {
+                titulo.Content = "Tiene que seleccionar un avatar para jugar.";
+            }
+
+            else
+            {
+                MainWindow ventanaJuego = new MainWindow(imagen);
+                ventanaJuego.Show();
+                this.Close();
+            }
+        }
+
+        private void aumentar(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 87;
+            da.To = 100;
+            da.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+            ((Button)sender).BeginAnimation(Button.HeightProperty, da);
+           
+            DoubleAnimation da2 = new DoubleAnimation();
+            da2.From = 259;
+            da2.To = 300;
+            da2.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+            ((Button)sender).BeginAnimation(Button.WidthProperty, da2);
+        }
+
+        private void reducir(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 100;
+            da.To = 87;
+            da.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+            ((Button)sender).BeginAnimation(Button.HeightProperty, da);
+
+            DoubleAnimation da2 = new DoubleAnimation();
+            da2.From = 300;
+            da2.To = 259;
+            da2.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+            ((Button)sender).BeginAnimation(Button.WidthProperty, da2);
+
+        }
+
+        private void seleccionarImagen(object sender, MouseButtonEventArgs e)
+        {
+            imagen.Opacity= 0.5;
+            imagen = ((Image)sender);
+            
+        }
+
+        private void subeOpacidad(object sender, MouseEventArgs e)
+        {
+            ((Image)sender).Opacity = 1;
+        }
+
+        private void bajaOpacidad(object sender, MouseEventArgs e)
+        {
+            if (imagen.Source != ((Image)sender).Source)
+            {
+                ((Image)sender).Opacity = 0.5;
+            }
+
+        }
+
+         
+
+        
+	}
+}
