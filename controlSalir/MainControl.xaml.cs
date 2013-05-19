c4b20727eb24a31b12ceb47c00707625c5926b67
emerrefe <mcarmenraflo@gmail.com>
2013-05-19 16:49:00 +0200

    version 7

---
+++ controlSalir/MainControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace controlSalir
+{
+	/// <summary>
+	/// Lógica de interacción para MainControl.xaml
+	/// </summary>
+	public partial class MainControl
+	{
+
+
+        private Brush colorBoton;
+        private LinearGradientBrush colorFondoInicial;
+
+        public Brush ColorBoton {
+            get { return rectanguloExterior.Fill; }
+            
+            set {   colorBoton = value;
+                    rectanguloExterior.Fill = value; }
+        
+        }
+
+        public LinearGradientBrush ColorIniBoton
+        {
+            get { return (LinearGradientBrush)rectanguloExterior.Fill; }
+
+            set
+            {
+                colorFondoInicial = value;
+                rectanguloExterior.Fill = value;
+            }
+
+        }
+
+
+
+        public MainControl()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+	}
+}
